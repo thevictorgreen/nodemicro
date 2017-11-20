56a37c099442fb92187a10a99f2217170fda7b5c
thevictorgreen <thevictorgreen@gmail.com>
2017-11-20 00:31:44 -0500

    mydb

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 var restify  = require('restify');
 var mysql    = require('mysql');
 
-var dbhost = process.env.MYDB_SERVICE_HOST;
+//var dbhost = process.env.MYDB_SERVICE_HOST;
 
 var pool     =    mysql.createPool({
     connectionLimit : 100, //important
-    host     : dbhost,
+    host     : 'mydb',
     user     : 'police',
     password : 'g0th@m',
     database : 'crimesdc',
